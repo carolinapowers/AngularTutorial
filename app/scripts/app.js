7d289879048fe54c342e1b1c5b8be857ae04d426
Carolina Powers <carolinapoloni@gmail.com>
2015-03-08 00:21:30 -0500

    the name of the directory is different than the one in the tutorial

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTutorialApp
+ * @description
+ * # angularTutorialApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+    .module('angularTutorialApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ])
+    .constant('FIREBASE_URL', 'https://boiling-fire-9739.firebaseio.com/');
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/posts.html',
+            controller: 'PostsCtrl'
+        })
+});
